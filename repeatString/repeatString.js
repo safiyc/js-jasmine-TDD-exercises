46185eb1074d7801dcf19483454db22760b3991f
Safiy Cham <csafiy@yahoo.com>
2018-06-24 15:37:25 -0700

    Create function to pass repeat string tests.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-var repeatString = function() {
+var repeatString = function(str, num) {
+    let arr = [];
+    let joinedArr;
 
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 1; i <= num; i++) {
+        arr.push(str);
+    }
+
+    joinedArr = arr.join("");
+    return joinedArr;
 }
 
 module.exports = repeatString

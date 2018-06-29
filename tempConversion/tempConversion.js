ff5e3cb23134a75d5fcf7d6244b33352b33d682c
Safiy Cham <csafiy@yahoo.com>
2018-06-28 18:16:59 -0700

    Create functions to convert F to C and C to F degrees.

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-var ftoc = function() {
-  
+var ftoc = function(deg) {
+  let result = (deg - 32) * .5556;
+  // below is to round to neares 1 decimal place, when needed
+  // how rounding code line works, not sure
+  // exponnential notation: 1.005e2 => 1.01
+  return Number(Math.round(result + 'e' + 1) + 'e-' + 1);
 }
 
-var ctof = function() {
-  
+var ctof = function(deg) {
+  let result = deg * (9/5) + 32;
+  return Number(Math.round(result + 'e' + 1) + 'e-' + 1);
 }
 
 module.exports = {
